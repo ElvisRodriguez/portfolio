78108fc84b941820b453d6e273d2ee014592fe08
ElvisRodriguez <elvisrodriguez1992@gmail.com>
2021-08-26 20:46:43 -0400

    Removed react markdown boilerplate from readme, added materialize css framework

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import React, { useEffect } from 'react';
+import M from "materialize-css";
+
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    M.AutoInit();
+  });
+
   return (
     <div className="App">
       <p>
