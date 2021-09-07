fc50bdecb6d95cf8babf3db34df48521e21cd7eb
ElvisRodriguez <elvisrodriguez1992@gmail.com>
2021-09-07 12:04:20 -0400

    Refactored education section in order to reuse existing components and reduce redundancy

--- src/Components/Education.js
+++ src/Components/Education.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import M from "materialize-css";
 
+import ProjectCard from './ProjectCard';
+
 import mec_logo from "../Images/mec-logo.jpg";
 import Strings from "../Utils/Strings.json";
 
@@ -14,39 +16,14 @@ function Education(props) {
             <div className="green-header">
                 <h1><code>Education</code></h1>
             </div>
-            <div className="card row">
-                <div className="card-content col l10 xl10">
-                    <div className="card-heading">
-                        <div className="card-title left-align">
-                            <span>CUNY - Medgar Evers College</span>
-                            <span>&nbsp;|&nbsp;</span>
-                            <small>
-                                BS Computer Science
-                            </small>
-                        </div>
-                        <div className="card-date left-align">2016 - 2021</div>
-                    </div>
-                    <ul className="card-badges">
-                        <li>ACM President</li>
-                        <li>ACM-W Vice President</li>
-                        <li>CUNY Tech Prep Alumnus</li>
-                        <li>Google CSSI Alumnus</li>
-                        <li>3.4 GPA</li>
-                    </ul>
-                    <ul className="card-details left-align">
-                        {Strings["MEC"].map((description, i) => {
-                            return(
-                            <li key={i}>
-                                <i className="material-icons">chevron_right</i>
-                                <p>{description}</p>
-                            </li>);
-                        })}
-                    </ul>
-                </div>
-                <div className="card-image col l2 xl2">
-                    <img src={mec_logo} className="mec-logo right-align valign-wrapper" alt="Medgar Evers College Logo"></img>
-                </div>
-            </div>
+            <ProjectCard
+                title="CUNY - Medgar Evers College"
+                displayLink="no-display"
+                smalls="BS Computer Science"
+                workPeriod="2016 - 2021"
+                badges={["ACM President", "ACM-W Vice President", "CUNY Tech Prep Alumnus", "Google CSSI Alumnus", "3.4 GPA"]}
+                descriptions={Strings["MEC"]}
+                image={mec_logo} alt="Medgar Evers College Logo"/>
         </div>
     );
 }
